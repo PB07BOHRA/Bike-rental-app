ee4f794f78fb9c1d9bbdd1db90bc793970af4ac4
PB07BOHRA <priyeshbohra1@gmail.com>
2026-07-16 03:49:35 +0530

    Implement login navigation and keyboard scrolling

--- app/register.tsx
+++ app/register.tsx
@@ -1,4 +1,15 @@
-import { StyleSheet, Text, View, Image, Pressable ,Alert,TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  Alert,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { router } from "expo-router";
 
 
@@ -44,9 +55,14 @@ if (phoneNumber.trim() === "") {
  Alert. alert("Success","Registration Successful!");
 
 };
-  return (
-<View style={styles.container}>
- 
+  return ( <KeyboardAvoidingView
+    style={{ flex: 1 }}
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+  >
+<ScrollView
+  contentContainerStyle={styles.container}
+  showsVerticalScrollIndicator={false}
+>
   
   <Image
   source={require("../assets/images/logo.png")}
@@ -107,17 +123,26 @@ placeholder="Confirm Your Password"
   >
   <Text style={styles.registerText}>Register</Text>
 </Pressable>
-</View>
+
+<Text style={styles.bottomText}>Already have an account?</Text>
+<Pressable
+ 
+  onPress={() => router.push("/login")}
+>
+  <Text style={styles.loginLinkText}>Login</Text>
+</Pressable>
+</ScrollView></KeyboardAvoidingView>
   );
 
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    paddingHorizontal: 25,
-    paddingTop: 40,
-  },
+ container: {
+  flexGrow: 1,
+  backgroundColor: "#F5F5F5",
+  paddingHorizontal: 25,
+  paddingTop: 40,
+  paddingBottom: 30,
+},
 
   logo: {
     width: 140,
@@ -138,12 +163,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
     color: "#333",
-    marginBottom: 8,
-    marginTop: 12,
+    marginBottom: 4,
+    marginTop: 8,
   },
 
   input: {
-    height: 55,
+    height: 48,
     backgroundColor: "#FFFFFF",
     borderRadius: 10,
     borderWidth: 1,
@@ -154,11 +179,11 @@ const styles = StyleSheet.create({
 
   registerButton: {
     backgroundColor: "#2563EB",
-    height: 55,
+    height:55,
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 10,
-    marginTop: 30,
+    marginTop: 15,
   },
 
   registerText: {
@@ -168,17 +193,18 @@ const styles = StyleSheet.create({
   },
 
   bottomText: {
-    marginTop: 25,
+    marginTop: 12,
     textAlign: "center",
     fontSize: 15,
     color: "#555",
   },
 
-  loginLink: {
+
+  loginLinkText: {
     textAlign: "center",
-    color: "#2563EB",
-    fontSize: 16,
-    fontWeight: "bold",
-    marginTop: 6,
-  },
+  color: "#2563EB",
+  fontSize: 16,
+  fontWeight: "bold",
+  marginTop: 6,
+},
 });
