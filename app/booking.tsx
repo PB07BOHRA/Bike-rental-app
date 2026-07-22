3eaa727f0a9005803e19284ecd0d5aa25b67af66
PB07BOHRA <priyeshbohra1@gmail.com>
2026-07-22 20:42:43 +0530

    Add booking screen

---
+++ app/booking.tsx
@@ -0,0 +1,18 @@
+import{Text,View,StyleSheet,} from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+const bike =[{
+    id: 1,
+    name: "Royal Enfield Classic 350",
+    price: 1300,
+    rating: "⭐4.8",
+    image: require("../../assets/images/royal350.png"),
+},
+{
+    id: 2,
+    name: "Royal Enfield Himalayan 450",
+    price: 1400,
+    rating: "⭐4.8",
+    image: require("../../assets/images/royal350.png"),
+},
+]
